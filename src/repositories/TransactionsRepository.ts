45352d2073030a4017068597718d5c0636e35d6c
Debora de Oliveira <doliveira@cobasi.com.br>
2020-08-09 18:05:31 -0300

    implementa desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string
+  value: number
+  type: 'income' | 'outcome'
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,33 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((output, each) => {
+      if (each.type === 'income') {
+        output = output + each.value
+      }
+      return output
+    }, 0)
+
+    const outcome = this.transactions.reduce((output, each) => {
+      if (each.type === 'outcome') {
+        output = output + each.value
+      }
+      return output
+    }, 0)
+
+    const total = income - outcome
+
+    return { income, outcome, total }
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type })
+    this.transactions.push(transaction)
+    return transaction
   }
 }
 
